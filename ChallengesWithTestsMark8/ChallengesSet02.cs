f99e560d185d408d318b158cf12dc96570d65700
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-03-19 04:46:32 -0700

    Initial commit

---
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet02
+    {
+        public bool CharacterIsALetter(char c)
+        {
+            return char.IsLetter(c);
+        }
+        public bool CharacterIsNotALetter(char enumerator)
+        {
+            return enumerator == char.MaxValue;
+        }
+
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+            return vals.Length % 2 == 0;
+        }
+
+        public bool IsNumberEven(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNumberOdd(int num)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Sum(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumEvens(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSumOdd(List<int> numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
